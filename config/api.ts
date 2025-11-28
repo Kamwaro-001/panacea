33861cd98975520c45a87737dbd20d1275adc0c2
Job Kamwaro <jobkamwaro9@gmail.com>
2025-11-28 23:13:30 +0300

    fix wrong icon path name and switch to apk file for preview builds

--- config/api.ts
+++ config/api.ts
@@ -1,5 +1,5 @@
 // API Configuration
 // Change this URL based on your environment
 // export const API_URL = "http://localhost:3000";
-// export const API_URL = "http://192.168.100.6:3000"; // for network access
-export const API_URL = "https://panacea-api-5qmq.onrender.com"; // current live server
+export const API_URL = "http://192.168.100.6:3000"; // for network access
+// export const API_URL = "https://panacea-api-5qmq.onrender.com"; // current live server
